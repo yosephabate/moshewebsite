51c30dc34fe0d849a7137c684911e34fe651bc19
yisehakbogale <yisehakb@gmail.com>
2019-03-07 22:46:07 +0300

    add navbar animation

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,36 +1,56 @@
+$(document).ready(function () {
+
+    var toggleAffix = function (affixElement, scrollElement, wrapper) {
+
+        var height = affixElement.outerHeight(),
+            top = wrapper.offset().top;
+
+        if (scrollElement.scrollTop() >= top) {
+            wrapper.height(height);
+            affixElement.addClass("affix");
+            $("#logo").css("height", "60px");
+            $("#logo").css("margin", "-15px");
+        } else {
+            affixElement.removeClass("affix");
+            wrapper.height('auto');
+            $("#logo").css("height", "65px");
+            $("#logo").css("margin", "15px, -15px");
+        }
+    };
 
 
-// $(document).ready(function(){
-//     var scrollTop = 0;
-//     $(window).scroll(function(){
-//         scrollTop = $(window).scrollTop();
-//         $('.counter').html(scrollTop);
-//
-//         if (scrollTop >= 100) {
-//             $('#global-nav').addClass('scrolled-nav');
-//         } else if (scrollTop < 100) {
-//             $('#global-nav').removeClass('scrolled-nav');
-//         }
-//
-//     });
-//
-// });
-//scroll slides on swipe for touch enabled devices
+    $('[data-toggle="affix"]').each(function () {
+        var ele = $(this),
+            wrapper = $('<div></div>');
 
-$("#myCarousel").on("touchstart", function(event){
+        ele.before(wrapper);
+        $(window).on('scroll resize', function () {
+            toggleAffix(ele, $(this), wrapper);
+        });
+
+        // init
+        toggleAffix(ele, $(window), wrapper);
+    });
+});
+
+
+$("#myCarousel").on("touchstart", function (event) {
 
     var yClick = event.originalEvent.touches[0].pageY;
-    $(this).one("touchmove", function(event){
+    $(this).one("touchmove", function (event) {
 
         var yMove = event.originalEvent.touches[0].pageY;
-        if( Math.floor(yClick - yMove) > 1 ){
+        if (Math.floor(yClick - yMove) > 1) {
             $(".carousel").carousel('next');
-        }
-        else if( Math.floor(yClick - yMove) < -1 ){
+        } else if (Math.floor(yClick - yMove) < -1) {
             $(".carousel").carousel('prev');
         }
     });
-    $(".carousel").on("touchend", function(){
+    $(".carousel").on("touchend", function () {
         $(this).off("touchmove");
     });
+});
+
+$('#myCarousel').bind('slide.bs.carousel', function (e) {
+    console.log('slide event!');
 });
