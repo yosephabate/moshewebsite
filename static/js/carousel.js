3d1769b9344344e28c6c73c1f347fea6a7375143
yisehakbogale <yisehakb@gmail.com>
2019-03-07 19:56:07 +0300

    add custom css

--- static/js/carousel.js
+++ static/js/carousel.js
@@ -1,6 +1,20 @@
 
 
-
+// $(document).ready(function(){
+//     var scrollTop = 0;
+//     $(window).scroll(function(){
+//         scrollTop = $(window).scrollTop();
+//         $('.counter').html(scrollTop);
+//
+//         if (scrollTop >= 100) {
+//             $('#global-nav').addClass('scrolled-nav');
+//         } else if (scrollTop < 100) {
+//             $('#global-nav').removeClass('scrolled-nav');
+//         }
+//
+//     });
+//
+// });
 //scroll slides on swipe for touch enabled devices
 
 $("#myCarousel").on("touchstart", function(event){
